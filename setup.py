d2112a037495923d1a4db7f34e6f0a65141adce3
Dan <dan.pinzaru93@gmail.com>
2018-08-25 17:23:47 +0100

    New Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name = 'symmetry_py',
     packages = find_packages(),
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'Symmetry',
     license = 'MIT',
     author = 'Logistio Software',
